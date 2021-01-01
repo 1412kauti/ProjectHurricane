57f6364f420b3d2164623323ae1030a5a79022df
Kautilya Reddy <kautilya97@gmail.com>
2021-01-01 14:45:49 +0530

    2021 fix file tree access v2

---
+++ UI/money.py
@@ -0,0 +1,259 @@
+import sys
+from PyQt5 import QtWidgets
+from test2 import dataz
+from StartScreen import Ui_Form1
+from Login_Screen import Ui_Form2
+from Registration_Screen import Ui_Form3
+from User_Card import Ui_Form4
+from User_Paypal import Ui_Form5
+from Driver_Card import Ui_Form6
+from Driver_Paypal import Ui_Form7
+from Thank_You import Ui_Form8
+from Verification_Screen import Ui_Form9
+from Congratulations import Ui_Form10
+from forgotChoices import Ui_Form11
+from changePassword import Ui_Form12
+from UserScreen import Ui_Form13
+from BackEnd import BackEnd
+from random import randint
+import numpy as np
+import sqlite3
+conn = sqlite3.connect("assessment2.db")
+c = conn.cursor()
+print("connected2")
+        
+class UserMainScreen(QtWidgets.QWidget):
+    def __init__(self):
+        super(UserMainScreen,self).__init__()
+        self.ui13 = Ui_Form13()
+        self.ui13.setupUi(self)
+class changePassword(QtWidgets.QWidget):
+    def __init__(self):
+        super(changePassword,self).__init__()
+        self.ui12 = Ui_Form12()
+        self.ui12.setupUi(self)
+        self.ui12.changePassword.clicked.connect(self.clos)
+    def clos(self):
+        self.close()
+class forgotChoices(QtWidgets.QWidget):
+    def __init__(self):
+        super(forgotChoices,self).__init__()
+        self.ui11 = Ui_Form11()
+        self.ui11.setupUi(self)
+        self.ui11.verify_button.clicked.connect(self.verify)
+    def verify(self):
+        self.o10 = changePassword()
+        self.o10.show()
+        self.close()
+class Congratulations(QtWidgets.QWidget):
+    def __init__(self):
+        super(Congratulations,self).__init__()
+        self.ui10 = Ui_Form10()
+        self.ui10.setupUi(self)
+        self.ui10.go2Login.clicked.connect(self.back2Login)
+    def back2Login(self):
+        self.o9 = LoginScreen()
+        self.o9.show()
+        self.close()
+class Verification_Screen(QtWidgets.QWidget):
+    def __init__(self):
+        super(Verification_Screen,self).__init__()
+        self.ui9 = Ui_Form9()
+        self.ui9.setupUi(self)
+        self.ui9.Complete_Verification.clicked.connect(self.go_to_Account_Activated)
+
+    def go_to_Account_Activated(self):
+        self.o8 = Congratulations()
+        self.o8.show()
+        self.close()
+class Thank_You(QtWidgets.QWidget):
+    def __init__(self):
+        super(Thank_You,self).__init__()
+        a = randint(0,1000)
+        self.ui8 = Ui_Form8()
+        self.ui8.setupUi(self)
+        self.ui8.Verify_Now.clicked.connect(self.open_Verification_Screen)
+        self.ui8.Security_Code.setText("%d"%a)
+        # self.ui8.Security_Code.setText(self.Security_generator)
+    def open_Verification_Screen(self):
+        self.o7 = Verification_Screen()
+        self.o7.show()
+        self.close()  
+class Driver_Paypal(QtWidgets.QWidget):
+    def __init__(self):
+        super(Driver_Paypal,self).__init__()
+        self.ui7 = Ui_Form7()
+        self.ui7.setupUi(self)
+        self.ui7.Submit_Details4.clicked.connect(self.open_Thankyou)
+    def open_Thankyou(self):
+        self.o6 = Thank_You()
+        self.o6.show()
+        self.close()
+        self.take_Driver_Paypal_Inputs()
+    def take_Driver_Paypal_Inputs(self):
+        driver_paypal_payme = self.ui7.Driver_Paypal_payme.text()
+        driver_card_name = 0
+        driver_card_account_number = 0
+        driver_sort_code = 0
+        self.d5 = dataz()
+        self.d5.Insert_into_driver_payments(driver_card_name,driver_card_account_number,driver_sort_code,driver_paypal_payme) 
+class Driver_Card(QtWidgets.QWidget):
+    def __init__(self):
+        super(Driver_Card,self).__init__()
+        self.ui6 = Ui_Form6()
+        self.ui6.setupUi(self)
+        self.ui6.Submit_Details3.clicked.connect(self.open_Thankyou)
+    def open_Thankyou(self):
+        self.o6 = Thank_You()
+        self.o6.show()
+        self.close()
+        self.take_Driver_Card_Inputs()
+    def take_Driver_Card_Inputs(self):
+        driver_card_name = self.ui6.Drive_Card_Name.text()
+        driver_card_account_number = self.ui6.Driver_card_account_number.text()
+        driver_sort_code1 = self.ui6.Driver_Card_SortCode1.text()
+        driver_sort_code2 = self.ui6.Driver_Card_SortCode2.text()
+        driver_sort_code3 = self.ui6.Driver_Card_SortCode3.text()
+        driver_sort_code = str(driver_sort_code1) + "-" + str(driver_sort_code2) + "-" + str(driver_sort_code3)
+        driver_paypal_payme = 0
+        self.d4 = dataz()
+        self.d4.Insert_into_driver_payments(driver_card_name,driver_card_account_number,driver_sort_code,driver_paypal_payme) 
+class User_Paypal(QtWidgets.QWidget):
+    def __init__(self):
+        super(User_Paypal,self).__init__()
+        self.ui5 = Ui_Form5()
+        self.ui5.setupUi(self)
+        self.ui5.Submit_Details2.clicked.connect(self.open_Thankyou)
+    def open_Thankyou(self):
+        self.o6 = Thank_You()
+        self.o6.show()
+        self.close()
+        self.take_User_Paypal_Inputs()
+    def take_User_Paypal_Inputs(self):
+        user_paypal_email =  self.ui5.User_Paypal_Email.text()
+        user_paypal_password = self.ui5.User_Paypal_Password.text()
+        user_card_name = 0
+        user_card_number = 0
+        user_card_cvv = 0
+        self.d3 = dataz()
+        self.d3.Insert_into_customer_payments(user_card_name,user_card_number,user_card_cvv,user_paypal_email,user_paypal_password)
+class User_Card(QtWidgets.QWidget):
+    def __init__(self):
+        super(User_Card,self).__init__()
+        self.ui4 = Ui_Form4()
+        self.ui4.setupUi(self)
+        self.ui4.Submit_Details1.clicked.connect(self.open_Thankyou)
+    def open_Thankyou(self):
+        self.o6 = Thank_You()
+        self.o6.show()
+        self.close()
+        self.take_User_Card_Inputs()
+    def take_User_Card_Inputs(self):
+        user_card_name = self.ui4.User_Card_Name.text()
+        user_card_number = self.ui4.User_Card_Number.text()
+        user_card_cvv = self.ui4.User_Card_CVV.text()
+        user_paypal_email = 0
+        user_paypal_password = 0
+        self.d2=dataz()
+        self.d2.Insert_into_customer_payments(user_card_name,user_card_number,user_card_cvv,user_paypal_email,user_paypal_password)
+class RegisterScreen(QtWidgets.QWidget):
+    def __init__(self):
+        super(RegisterScreen,self).__init__()
+        self.ui3 = Ui_Form3()
+        self.ui3.setupUi(self)
+        self.ui3.User_Submit_Button.clicked.connect(self.User_Payment)
+        self.ui3.Driver_Submit_Button.clicked.connect(self.Driver_Payment)
+    def take_driver_inputs(self):
+        driver_first_name = self.ui3.Driver_First_Name_Text.text()
+        driver_last_name = self.ui3.Driver_Last_Name_Text.text()
+        driver_email = self.ui3.Driver_Email_Text.text()
+        driver_password = self.ui3.Driver_Password_Text.text()
+        driver_contact_number = self.ui3.Driver_Phone_Number_Text.text()
+        driver_license = self.ui3.Driver_License_Number.text()
+        driver_car_license = self.ui3.Driver_Car_Number.text()
+        driver_car_make = self.ui3.Driver_Car_Make_Text.text()
+        driver_car_color = self.ui3.Driver_Car_Color.text()
+        driver_payment = self.ui3.comboBox_2.currentText()
+        self.d2=dataz()
+        self.d2.Insert_Into_drivers(driver_first_name,driver_last_name,driver_email,driver_password,driver_contact_number,driver_license,driver_car_license,driver_car_make,driver_car_color,driver_payment)
+    def take_user_inputs(self):
+        user_first_name = self.ui3.User_First_Name_Text.text()
+        user_last_name = self.ui3.User_Last_Name_Text.text()
+        user_email = self.ui3.User_Email_Text.text()
+        user_contact_number = self.ui3.User_Contact_Number_Text.text()
+        user_password = self.ui3.User_Password_Text.text()
+        user_payment = self.ui3.comboBox.currentText()
+        self.d1=dataz()
+        self.d1.Insert_Into_customers(user_first_name,user_last_name,user_email,user_contact_number,user_password,user_payment)
+    def open_User_Card(self):
+        self.o2 = User_Card()
+        self.o2.show()
+        self.close()
+    def open_User_Paypal(self):
+        self.o3 = User_Paypal()
+        self.o3.show()
+        self.close()
+    def open_Driver_Card(self):
+        self.o4 = Driver_Card()
+        self.o4.show()
+        self.close()
+    def open_Driver_Paypal(self):
+        self.o5 = Driver_Paypal()
+        self.o5.show()
+        self.close()
+    def User_Payment(self):
+        self.take_user_inputs()
+        if self.ui3.User_Password_Text.text() == self.ui3.User_Confirm_Password_Text.text():
+            if self.ui3.comboBox.currentText()=="Card":
+                self.ui3.User_Submit_Button.clicked.connect(self.open_User_Card)
+            elif self.ui3.comboBox.currentText()=="PayPal":
+                self.ui3.User_Submit_Button.clicked.connect(self.open_User_Paypal)
+        else:
+            self.ui3.User_Invalid_Password.setText("Passwords Mismatch, try again")
+    def Driver_Payment(self):
+        self.take_driver_inputs()
+        if self.ui3.Driver_Password_Text.text() == self.ui3.Driver_Confirm_Password_Text.text():
+            if self.ui3.comboBox_2.currentText()=="Card":
+                self.ui3.Driver_Submit_Button.clicked.connect(self.open_Driver_Card)
+            elif self.ui3.comboBox_2.currentText()=="PayPal":
+                self.ui3.Driver_Submit_Button.clicked.connect(self.open_Driver_Paypal)
+        else:
+            self.ui3.Driver_Invalid_Password.setText("Passwords Mismatch, try again")
+class LoginScreen(QtWidgets.QWidget):
+    def __init__(self):
+        super(LoginScreen,self).__init__()
+        self.ui2 = Ui_Form2()
+        self.ui2.setupUi(self)
+        self.ui2.user_ForgotPass.clicked.connect(self.forgetPass)
+        self.ui2.driver_ForgotPass.clicked.connect(self.forgetPass)
+        self.ui2.user_Submit.clicked.connect(self.openLogin)
+    def forgetPass(self):
+        self.o11 = forgotChoices()
+        self.o11.show()
+    def openLogin(self):
+        self.o12 = UserMainScreen()
+        self.o12.show()
+        self.close()
+class StartScreen(QtWidgets.QWidget):
+    def __init__(self):
+        super(StartScreen,self).__init__()
+        self.ui1 = Ui_Form1()
+        self.ui1.setupUi(self)
+        self.ui1.Login_Button.clicked.connect(self.open_Login)
+        self.ui1.Register_Button.clicked.connect(self.open_Register)    
+    def open_Login(self):
+        self.o1 = LoginScreen()
+        self.o1.show()
+        self.close()
+    def open_Register(self):
+        self.o2 = RegisterScreen()
+        self.o2.show()
+        self.close()
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = StartScreen()
+    application.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ UI/test2.py
@@ -0,0 +1,431 @@
+import sqlite3
+conn = sqlite3.connect("/home/hao/Documents/TaxiBookingSystemPythonAss2/UI/assessment2.db")
+c = conn.cursor()
+print("hello")
+class dataz():
+    @staticmethod
+    def Create_Table():
+        c.execute("""CREATE TABLE table_name""")
+        conn.commit()
+
+    @staticmethod
+    def Rename_Table():
+        import sqlite3
+        conn = sqlite3.connect("assessment2.db")
+        c = conn.cursor()
+        c.execute("ALTER TABLE trip RENAME TO journey")        
+        conn.commit()
+    @staticmethod
+    def Drop_Table():
+        c.execute("DROP TABLE table_name")
+        conn.commit()
+
+    @staticmethod
+    def Insert_Into_customers(z,y,x,w,v,u):
+        import sqlite3
+        conn = sqlite3.connect("assessment2.db")
+        c = conn.cursor()
+        c.execute('''PRAGMA journal_mode = WAL''')
+        c.execute("INSERT INTO customers (first_name,last_name,email,phone_number,password,payment_method) VALUES(?,?,?,?,?,?)",(z,y,x,w,v,u))
+        conn.commit()
+    
+    @staticmethod    
+    def Insert_Into_drivers(z,y,x,w,v,u,t,s,r,q):
+        import sqlite3
+        conn = sqlite3.connect("assessment2.db")
+        c = conn.cursor()
+        c.execute('''PRAGMA journal_mode = WAL''')
+        c.execute("""INSERT INTO drivers
+        (first_name, last_name, email, phone_number, password, car_make, car_colour, car_license_plate_number, license_expiry, driver_license) VALUES (?,?,?,?,?,?,?,?,?,?)""", (z,y,x,w,v,u,t,s,r,q))
+        conn.commit()
+
+    @staticmethod
+    def Insert_Into_admins():
+        c.execute("""INSERT INTO admins
+        (first_name, last_name, email, phone_number, password) VALUES ('haos', 'zhong', 'hao123', 07412566921, 'password')""")
+        conn.commit()
+
+    @staticmethod
+    def Insert_Into_journey():
+        c.execute("""INSERT INTO journey
+        (order_ID, date_created, customer_ID, driver_name, status, driver_ID, start_point, end_point, price, customer_number) VALUES(23, 'someday', 1, 'driver', 'finished', 5, 'somewhere', 'somewhere_else', 42, 23)""")
+        conn.commit()
+
+    @staticmethod
+    def Insert_into_customer_payments(z,y,x,w,v):
+        import sqlite3
+        conn = sqlite3.connect("assessment2.db")
+        c = conn.cursor()
+        c.execute('''PRAGMA journal_mode = WAL''')
+        c.execute("""INSERT INTO customer_payment
+        (name, card_number, cvv, email, password)
+        VALUES (?,?,?,?,?)""", (z,y,x,w,v))
+        conn.commit()
+
+    @staticmethod
+    def Insert_into_driver_payments(z,y,x,w,v,u):
+        import sqlite3
+        conn = sqlite3.connect("assessment2.db")
+        c = conn.cursor()
+        c.execute("""INSERT INTO driver_payments
+        (name, account_number, sort_code, payme_link)
+        VALUES (?,?,?,?,?,?)""", (z,y,x,w,v,u))
+        conn.commit()    
+
+    @staticmethod
+    def Delete_Duplicates_Customers():
+        c.execute("""DELETE FROM customers
+        WHERE rowid NOT IN (SELECT min(rowid) 
+        FROM customers GROUP BY first_name,last_name,email,password,phone_number,payment_method)""")
+        conn.commit()
+
+    @staticmethod
+    def Delete_Duplicates_Drivers():
+        c.execute("""DELETE FROM drivers
+        WHERE rowid NOT IN (SELECT min(rowid) 
+        FROM drivers GROUP BY first_name,last_name,email,phone_number,password,car_make,car_colour,car_license_plate_number,driver_license,license_expiry,bank_account)""")
+        conn.commit()
+
+    @staticmethod
+    def Delete_Duplicates_Admins():
+        c.execute("""DELETE FROM admins
+        WHERE rowid NOT IN (SELECT min(rowid) 
+        FROM admins GROUP BY first_name,last_name,email,phone_number,password)""")
+        conn.commit()
+
+    @staticmethod
+    def Check_Duplicates_customers():
+        c.execute("""SELECT first_name,last_name,email,phone_number,password,payment_method, COUNT(*)
+        FROM customers
+        GROUP BY first_name,last_name,email,phone_number,password,payment_method
+        HAVING COUNT(*) > 1""")
+        print(c.fetchall())
+        conn.commit()
+
+    @staticmethod
+    def Check_Duplicates_drivers():
+        c.execute("""SELECT first_name,last_name,email,phone_number,password,car_make,car_colour,car_license_plate_number,driver_license,license_expiry COUNT(*)
+        FROM drivers
+        GROUP BY first_name,last_name,email,phone_number,password,car_make,car_colour,car_license_plate_number,driver_license
+        HAVING COUNT(*) > 1""")
+        print(c.fetchall())
+        conn.commit()
+
+    @staticmethod
+    def Update_Last_Name_customers():
+        c.execute("""UPDATE customers
+        SET last_name = 'someone'
+        WHERE user_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_Email_customers():
+        c.execute("""UPDATE customers
+        SET email = 'someone_email'
+        WHERE user_ID = 1""")
+        conn.commit()
+        
+    @staticmethod
+    def Update_phone_number_customers():
+        c.execute("""UPDATE customers
+        SET phone_number = '07412566921'
+        WHERE user_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_password_customers():
+        c.execute("""UPDATE customers
+        SET password = '07412566921'
+        WHERE user_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_payment_method_customers():
+        c.execute("""UPDATE customers
+        SET payment_method = '07412566921'
+        WHERE user_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_first_name_drivers():
+        c.execute("""UPDATE drivers
+        SET first_name = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_last_name_drivers():
+        c.execute("""UPDATE drivers
+        SET last_name = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_phone_number_drivers():
+        c.execute("""UPDATE drivers
+        SET phone_number = '07412566921'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_password_drivers():
+        c.execute("""UPDATE drivers
+        SET password = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_car_make_drivers():
+        c.execute("""UPDATE drivers
+        SET car_make = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_car_colour_drivers():
+        c.execute("""UPDATE drivers
+        SET car_colour = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_first_driver_license_drivers():
+        c.execute("""UPDATE drivers
+        SET driver_license = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_license_expiry_drivers():
+        c.execute("""UPDATE drivers
+        SET license_expiry = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_bank_account_drivers():
+        c.execute("""UPDATE drivers
+        SET bank_account = 'someone'
+        WHERE driver_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_first_name_admins():
+        c.execute("""UPDATE admins
+        SET first_name = 'someone'
+        WHERE employee_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_last_name_admins():
+        c.execute("""UPDATE admins
+        SET last_name = 'someone'
+        WHERE employee_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_email_admins():
+        c.execute("""UPDATE admins
+        SET email = 'someone'
+        WHERE employee_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_phone_number_admins():
+        c.execute("""UPDATE admins
+        SET phone_number = 'someone'
+        WHERE employee_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_password_admins():
+        c.execute("""UPDATE admins
+        SET password = 'someone'
+        WHERE employee_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_date_created_journey():
+        c.execute("""UPDATE journey
+        SET last_name = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+    
+    @staticmethod
+    def Update_customer_ID_journey():
+        c.execute("""UPDATE journey
+        SET customer_ID = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+
+    @staticmethod
+    def Update_driver_name_journey():
+        c.execute("""UPDATE journey
+        SET driver_name = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    
+    @staticmethod
+    def Update_status_journey():
+        c.execute("""UPDATE journey
+        SET status = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_driver_ID_journey():
+        c.execute("""UPDATE journey
+        SET driver_ID = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+
+    @staticmethod
+    def Update_start_piont_journey():
+        c.execute("""UPDATE journey
+        SET start_piont = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+
+    @staticmethod
+    def Update_end_piont_journey():
+        c.execute("""UPDATE journey
+        SET end_piont = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+
+    @staticmethod
+    def Update_price_journey():
+        c.execute("""UPDATE journey
+        SET price = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+
+    @staticmethod
+    def Update_customer_number_journey():
+        c.execute("""UPDATE journey
+        SET customer_number = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+        
+    @staticmethod
+    def Update_name_customer_payment():
+        c.execute("""UPDATE customer_payment
+        SET name = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_card_customer_payment():
+        c.execute("""UPDATE customer_payment
+        SET card_number = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_cvv_customer_payment():
+        c.execute("""UPDATE customer_payment
+        SET cvv = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_email_customer_payment():
+        c.execute("""UPDATE customer_payment
+        SET email = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_password_customer_payment():
+        c.execute("""UPDATE customer_payment
+        SET password = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_name_driver_payments():
+        c.execute("""UPDATE driver_payments
+        SET name = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_account_number_driver_payments():
+        c.execute("""UPDATE driver_payments
+        SET account_number = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_sort_code_driver_payments():
+        c.execute("""UPDATE driver_payments
+        SET sort_code = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_payme_link_driver_payments():
+        c.execute("""UPDATE driver_payments
+        SET payme_link = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Update_name_driver_payments():
+        c.execute("""UPDATE driver_payments
+        SET name = 'someone'
+        WHERE order_ID = 1""")
+        conn.commit()
+
+    @staticmethod
+    def Delete_Row_customers():
+        c.execute("DELETE FROM customers where rowid = 1")
+        conn.commit()
+
+    @staticmethod
+    def Delete_Row_drivers():
+        c.execute("DELETE FROM drivers where rowid = 1")
+        conn.commit()
+
+    @staticmethod
+    def Delete_Row_admins():
+        c.execute("DELETE FROM admins where rowid = 1")
+        conn.commit()
+
+    @staticmethod
+    def Delete_Row_journey():
+        c.execute("DELETE FROM journey where rowid = 1")
+        conn.commit()
+
+    @staticmethod
+    def Read_From_Customers():
+        c.execute("SELECT*FROM customers")
+        print(c.fetchall())
+        conn.commit()
+
+    @staticmethod
+    def Read_From_Drivers():
+        c.execute("SELECT*FROM drivers")
+        print(c.fetchall())
+        conn.commit()
+
+    @staticmethod
+    def Read_From_Admins():
+        c.execute("SELECT*FROM admins")
+        print(c.fetchall())
+        conn.commit()
+
+    @staticmethod
+    def Read_From_Journey():
+        c.execute("SELECT*FROM journey")
+        print(c.fetchall())
+        conn.commit()
+
+v = dataz()
+v.Insert_into_customer_payments(1,2,3,4,5)
