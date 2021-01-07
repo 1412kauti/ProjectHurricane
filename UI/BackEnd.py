9a118288c67f2b4330c483cc973128525527041c
nikitaSisikin <74993680+nikitaSisikin@users.noreply.github.com>
2021-01-07 07:00:53 +0000

    Add files via upload

--- UI/BackEnd.py
+++ UI/BackEnd.py
@@ -1,5 +1,7 @@
 from random import randint
 import sqlite3
+import haversine as hs
+import distanceCheck as dc
 
 class BackEnd(object):
 
@@ -8,40 +10,33 @@ class BackEnd(object):
 
 
 
-connection = sqlite3.connect("assessment2.db")
+    connection = sqlite3.connect("assessment2.db")
 
-sql_select_Query = "select * from map_of_locations"
-cursor = connection.cursor()
-cursor.execute(sql_select_Query)
-records = cursor.fetchall()
+    sql_select_Query = "select * from map_of_locations"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
 
-locations = {
-    'luton_mall' : (records[0][2], records[0][3]),
-    'luton_train_station' : (records[1][2], records[1][3]),
-    'uni' : (records[2][2], records[2][3]),
-    'stockwood_park' : (records[3][2], records[3][3]),
-    'wardown_house' : (records[4][2], records[4][3]),
-    'bedford_park' : (records[5][2], records[5][3]),
-    'barkers_lane' : (records[6][2], records[6][3]),
-    'jb_museum' : (records[7][2], records[7][3]),
-    'light_pyramid' : (records[8][2], records[8][3]),
-    'milton_uni_hospital' : (records[8][2], records[8][3]),
-    'milton_computer_museum' : (records[9][2], records[9][3]),
-    'milton_ikea' : (records[10][2], records[10][3])
-}
+    locations = {
+        'Luton mall' : (records[0][2], records[0][3]),
+        'Luton train station' : (records[1][2], records[1][3]),
+        'University of Bedfordshire' : (records[2][2], records[2][3]),
+        'Stockwood park' : (records[3][2], records[3][3]),
+        'Wardown house museum' : (records[4][2], records[4][3]),
+        'Bedford park' : (records[5][2], records[5][3]),
+        'Barkers lane' : (records[6][2], records[6][3]),
+        'John Bunyan Museum and Library' : (records[7][2], records[7][3]),
+        'Light Pyramid' : (records[8][2], records[8][3]),
+        'Milton Keynes University Hospital' : (records[8][2], records[8][3]),
+        'National Museum of Computing' : (records[9][2], records[9][3]),
+        'milton keynes IKEA' : (records[10][2], records[10][3])
+    }
 
-import haversine as hs
-import distanceCheck as dc
+    def priceCalc(loc1, loc2):
+            distance_km = dc.distanceCheck(loc1, loc2)
+            trip_price = distance_km * 5
+            #print(distance_km, trip_price)
+            return distance_km, trip_price
 
-def priceCalc(loc1, loc2):
-    if loc1 == loc2:
-        print("Locations can't be the same")
 
-    else:
-        distance_km = dc.distanceCheck(loc1, loc2)
-        trip_price = distance_km * 5
-        return format(trip_price, '.2f' ) + ' quid for ' + str(format(distance_km, '.2f')) + ' kilometers'
-
-# print(priceCalc(locations['luton_mall'], locations['stockwood_park']))
-# print(priceCalc(locations['milton_ikea'], locations['stockwood_park']))
-# print(priceCalc(locations['milton_ikea'], locations['milton_ikea']))
+BackEnd.priceCalc(BackEnd.locations['National Museum of Computing'], BackEnd.locations['University of Bedfordshire'])
