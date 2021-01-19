d4c7e3cf996459b47600210a3c111e2e9c4c5de6
grimereaper7471 <haoz0724@gmail.com>
2021-01-19 16:29:16 +0000

    changes

--- UI/test2.py
+++ UI/test2.py
@@ -51,7 +51,7 @@ class dataz():
         c = conn.cursor()
         c.execute('''PRAGMA journal_mode = WAL''')
         c.execute("""INSERT INTO journey
-        (Date, Time, Journey_ID, Start_Location, user_ID, customer_name, start_location, End_Location, driver_ID, driver_name, Car_Class, Car_Make, Car_Colour, Price, Distance, status) VALUES (?,?,?,?,?,?,?,?,?,?,?)""", (date,time,jID,start_point,end_point,driver_name,car_type,car_make,car_color,price,distance))
+        (Date, Time, Journey_ID, Start_Location, start_location, End_Location, driver_name, Car_Class, Car_Make, Car_Colour, Price, Distance) VALUES (?,?,?,?,?,?,?,?,?,?,?)""", (date,time,jID,start_point,end_point,driver_name,car_type,car_make,car_color,price,distance,))
         conn.commit()
 
     @staticmethod
